036bd33fe1379c400c953a97ef3742d74b6288e2
thopr <b6447333@gmail.com>
2020-11-04 19:58:44 +0300

    f

--- services/Api.js
+++ services/Api.js
@@ -3,14 +3,14 @@ import Cookies from "js-cookie";
 
 const siteUrl = Cookies.get("siteUrl");
 if (!siteUrl) {
-  Cookies.set("siteUrl", "https://rabwa.laqjewelry.com/wp-json/", {
+  Cookies.set("siteUrl", "https://167.99.246.8/wp-json/", {
     expires: 60,
   });
 }
 let urls = {
   development:
     "http://localhost/new_rabwa_project/rabwa-coubon/wordpress/wp-json/",
-  production: "https://rabwa.laqjewelry.com/wp-json/",
+  production: "https://167.99.246.8/wp-json/",
 };
 const api = Axios.create({
   baseURL: urls[process.env.NODE_ENV],
